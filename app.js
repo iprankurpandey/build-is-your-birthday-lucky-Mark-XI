35e3177a814ea9215f4b60a5b647d6e96dc9afe2
iprankurpandey <pprankur@gmail.com>
2021-08-27 12:45:23 +0530

    added the basic logic of app

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 const birthday = document.querySelector('#birthday')
-console.log(birthday.value)
-const luckynumber =document.querySelector('#luckynumber')
+const luckynumber = document.querySelector('#luckynumber')
 const button = document.querySelector('#button')
+const outputBox = document.querySelector('#outputbox')
 
 
-function checkbirthdayislucky(){
+function comparevalues(sum, luckynumber) {
 
-const dob = birthday.value;
-const sum =calculateSum(dob)
-console.log(sum)
-comparevalues (luckynumber.value,sum)
-}
-
-function comparevalues (luckynumber,sum){
-
-    if(sum%luckynumber == 0){
-        console.log("bday is lucky")
-    }
-    else{
-        console.log("bday is not lucky")
+    if (sum % luckynumber.value === 0) {
+        outputBox.innerText = "Your Birthday is Lucky";
+    } else {
+        outputBox.innerText = "Sorry, Better Luck Next Birth";
     }
 }
 
-function calculateSum(dob){
-    const dob= date.replaceAll("-","")
+
+
+
+
+function checkbirthdayislucky() {
+    var dob = birthday.value;
+    var sum = calculateSum(dob)
+    comparevalues(sum, luckynumber)
+}
+
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "")
     let sum = 0;
-    for (i=0;i<dob.length;i++){
-        sum = sum+ Number (charAt(i))
+    for (i = 0; i < dob.length; i++) {
+        sum = sum + Number(dob.charAt(i))
     }
     return sum;
 }
-button.addEventListener("click", comparevalues)
+button.addEventListener("click", checkbirthdayislucky)
